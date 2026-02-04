4bf7fc8801822bb57dd1ffc95b60267f2bcdbd16
mr-nobody-7 <vivekanandagodi@gmail.com>
2026-02-04 23:14:45 +0530

    add scroll progress indicator smooth scroll provider

---
+++ components/ScrollProgressIndicator.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { useEffect, useRef } from "react";
+
+export const ScrollProgressIndicator = () => {
+  const scrollBarRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (scrollBarRef.current) {
+        const { scrollHeight, clientHeight } = document.documentElement;
+        const scrollableHeight = scrollHeight - clientHeight;
+        const scrollY = window.scrollY;
+        const scrollProgress = (scrollY / scrollableHeight) * 100;
+
+        scrollBarRef.current.style.transform = `translateY(-${
+          100 - scrollProgress
+        }%)`;
+      }
+    };
+
+    handleScroll();
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="fixed top-[50vh] right-[2%] -translate-y-1/2 w-1.5 h-25 rounded-full bg-background-light overflow-hidden z-40">
+      <div
+        className="w-full bg-primary rounded-full h-full"
+        ref={scrollBarRef}
+      ></div>
+    </div>
+  );
+};
