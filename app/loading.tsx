cf341318f742f81b5a685c2e2c22db8d4af24767
mr-nobody-7 <vivekanandagodi@gmail.com>
2026-02-06 23:30:54 +0530

    add loading to the main page

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-4xl font-anton">Loading...</div>
+    </div>
+  );
+}
