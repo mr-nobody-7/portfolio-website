44a01f94f9a354ffd3b70724e464ee7c8dd28856
mr-nobody-7 <vivekanandagodi@gmail.com>
2026-04-08 23:47:30 +0530

    fix: keep custom cursor above open navigation drawer

--- components/CustomCursor.tsx
+++ components/CustomCursor.tsx
@@ -30,7 +30,7 @@ const CustomCursor = () => {
       width="27"
       height="30"
       viewBox="0 0 27 30"
-      className="hidden md:block fixed top-0 left-0 z-[50] pointer-events-none"
+      className="hidden md:block fixed top-0 left-0 z-[70] pointer-events-none"
       fill="none"
       id="cursor"
       strokeWidth="2"
@@ -43,6 +43,7 @@ const CustomCursor = () => {
       animate={{ opacity: 1 }}
       transition={{ duration: 0.3 }}
     >
+      <title>Custom cursor</title>
       <path
         d="M20.0995 11.0797L3.72518 1.13204C2.28687 0.258253 0.478228 1.44326 0.704999 3.11083L3.28667 22.0953C3.58333 24.2768 7.33319 24.6415 8.3792 22.7043C9.5038 20.6215 10.8639 18.7382 12.43 17.7122C13.996 16.6861 16.2658 16.1911 18.6244 15.9918C20.8181 15.8063 21.9811 12.2227 20.0995 11.0797Z"
         className="fill-foreground stroke-background/50"
